63fe780166d3175d5f2932726c3261916194e673
student <pawelwzietek@gmail.com>
2024-02-28 13:56:54 +0100

    Zad3

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(2.4, 5.5);
+        Point point = new Point(20.4, 50.5);
         //System.out.println(point.x + "," + point.y);
 
-        Segment seg = new Segment(point, new Point(1.7, 8.6));
+        Segment seg = new Segment(point, new Point(10.7, 80.6));
         //System.out.println(seg.getDistance());
         System.out.println(seg.toSvg());
+
+        Segment[] pSeg = Segment.perpendicularTo(seg, point);
+        System.out.println(pSeg[0].toSvg());
+        System.out.println(pSeg[1].toSvg());
     }
+
+
 }
--- svg/src/Segment.java
+++ svg/src/Segment.java
@@ -23,4 +23,14 @@ public class Segment {
     public String toSvg(){
         return String.format(Locale.ENGLISH,"<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:red;stroke-width:2\" />", start.x, start.y, end.x, end.y);
     }
+
+    public static Segment perpendicularTo(Segment s, Point p){
+        double dx = s.getEnd().x - s.getStart().x;
+        double dy = s.getEnd().y - s.getStart().y;
+
+        //return new Segment(p, new Point(p.x - dx, p.y - dy));
+        return new Segment[] {
+                new Segment((p, new Point(p.x - dy, p.y - dx)), (p, new Point(p.x + dy, p.y + dy))
+        };
+    }
 }
