be371fbde71912ccf61d5503c567364c7ff290f2
PawelBorowiecki <161360528+PawelBorowiecki@users.noreply.github.com>
2024-06-17 21:00:09 +0200

    Delete Kolokwium1-2022 directory

--- Kolokwium1-2022/src/main/java/org/example/AmbigiousProductException.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.List;
-
-public class AmbigiousProductException extends Exception{
-    public AmbigiousProductException(List<String> list) {
-        list.forEach(System.out::println);
-    }
-}
